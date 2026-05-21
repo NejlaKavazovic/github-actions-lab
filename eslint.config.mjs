35b2d889210b0d7e96ca74a99c22d8bba7cddb17
Nejla <nejla.kavazovic@fet.ba>
2026-05-21 19:15:18 +0200

    fix: ispravljen package.json i eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,17 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest
+      } 
+    } 
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 ]);
