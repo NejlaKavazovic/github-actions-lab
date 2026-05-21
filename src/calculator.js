c7669fd82c09dce0c003bf6acbf08b9712c8c1f0
Nejla <nejla.kavazovic@fet.ba>
2026-05-21 19:01:10 +0200

    Dodana sintaksna greška

---
+++ src/calculator.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  var c = 5;
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { add, subtract, multiply };
